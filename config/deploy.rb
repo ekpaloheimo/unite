831860331dbcd5ff2ee4fe77d426a0aaf028424a
Joni Toyryla <info@jonitoyryla.eu>
2016-02-23 15:54:06 +0200

    removed empty line

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,6 @@ require "rvm/capistrano"
 
 set :application, 'unite-the-armies'
 set :repo_url, 'git@unite.github.com:ekpaloheimo/unite-the-armies.git'
-
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'public/googleca9639854eea1a9b.html')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 set :keep_releases, 5

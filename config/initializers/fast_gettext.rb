db9bf478bb56528adcfb4909c4b0c6d9e64cc711
Joni Toyryla <info@jonitoyryla.eu>
2016-02-25 10:28:01 +0200

    new flags

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -3,7 +3,7 @@ I18n::Backend::Simple.send(:include, I18n::Backend::Fallbacks)
 
 #FastGettext.add_text_domain('unite-the-armies', :path => File.join(Rails.root,'config/locales'), :type => :po)
 FastGettext.text_domain = 'unite-the-armies'
-FastGettext.available_locales = ['en','es','fr','ar','ru','zh','fi']
+FastGettext.available_locales = ['en','es','fr','ar','ru','zh','fi','de','se']
 #FastGettext.available_locales = ['en']
 FastGettext.locale = 'en'
 FastGettext.add_text_domain('unite-the-armies', :path => File.join(Rails.root,'config/locales'))

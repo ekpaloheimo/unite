4a19eb198fd155c4584f8f70ccce3c7acbc3b72c
Jouni Tulkki <jouni@iwa.fi>
2016-10-23 22:21:28 +0300

    Update deploy configuration files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,4 @@
-server '188.117.25.189', user: 'ubuntu', roles: %w{app db web}
+server 'unite-the-armies.org', user: 'ubuntu', roles: %w{app db web}
 set :deploy_to, '/var/www/unite-the-armies.org'
 set :ssh_options, {  
-  keys: %w(/home/jto/.ssh/unite-the-armies.pem /home/jto/.ssh/id_rsa_ekpaloheimo_git)
 }
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,4 @@
-server '188.117.25.189', user: 'ubuntu', roles: %w{app db web}
+server '188.117.30.197', user: 'ubuntu', roles: %w{app db web}
 set :deploy_to, '/var/www/testi.unite-the-armies.org'
 set :ssh_options, {  
-  keys: %w(/home/jto/.ssh/unite-the-armies.pem /home/jto/.ssh/id_rsa_ekpaloheimo_git)
 }
